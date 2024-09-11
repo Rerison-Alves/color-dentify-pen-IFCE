7fb8183892077a65717ad54d1df494feef6d65fc
Victor <j1747victor@gmail.com>
2024-09-11 14:51:11 -0300

    modificações que fizemos no lab

--- include/colors.h
+++ include/colors.h
@@ -3,20 +3,20 @@
 
 #include "language.h"
 
-// Enum de cores em inglês
+// Enum de cores em inglês com valores específicos
 enum class Color {
-    BLACK,
-    WHITE,
-    GREY,
-    YELLOW,
-    BLUE,
-    CYAN,
-    ORANGE,
-    PINK,
-    PURPLE,
-    GREEN,
-    RED,
-    INVALID // Usado para representar uma cor inválida
+    BLACK = 0,
+    WHITE = 1,
+    GREY = 2,
+    YELLOW = 3,
+    BLUE = 4,
+    CYAN = 5,
+    ORANGE = 6,
+    PINK = 7,
+    PURPLE = 8,
+    GREEN = 9,
+    RED = 10,
+    INVALID = -1 // Usado para representar uma cor inválida
 };
 
 // Função para converter um enum Color para uma string de cor, considerando o idioma
--- src/bot.cpp
+++ src/bot.cpp
@@ -45,12 +45,10 @@ void Bot::waitMessage(void * pvParameters)
     {
         int numNewMessages = _bot.getUpdates(_bot.last_message_received + 1);
 
-        while (numNewMessages)
-        {
-            Serial.println("got response");
-            handleNewMessages(numNewMessages);
-            numNewMessages = _bot.getUpdates(_bot.last_message_received + 1);
-        }
+        Serial.println("got response");
+        handleNewMessages(numNewMessages);
+        
+        
 
         _bot_lasttime = millis();
     } 
@@ -58,144 +56,129 @@ void Bot::waitMessage(void * pvParameters)
 
 void Bot::handleNewMessages(int numNewMessages)
 {
-  for (int i = 0; i < numNewMessages; i++)
-  {
+  
    
-    if(_bot.messages[i].text=="play"){
-        _bot.sendMessage(_bot.messages[i].chat_id, "Que os jogos começem!!!");
-        _bot.sendMessage(_bot.messages[i].chat_id, "Antes de comerçarmos a brincadeira preciso te dá uma dica:");
+    if(_bot.messages[numNewMessages].text=="play"){
+
+        _bot.sendMessage(_bot.messages[numNewMessages].chat_id, "Que os jogos começem!!! \ 
+        Antes de comerçarmos a brincadeira preciso te dá uma dica:");
        
-        redGame(i);
-        purpleGame(i);
-        blueGame(i);
+        redGame(numNewMessages);
+        purpleGame(numNewMessages);
+        blueGame(numNewMessages);
         if(_lang==Language::PORTUGUESE){
-            _bot.sendMessage(_bot.messages[i].chat_id, "Meus parabens, vc conseguiu finalizar o meu jogo!!!!!");
+            _bot.sendMessage(_bot.messages[numNewMessages].chat_id, "Meus parabens, vc conseguiu finalizar o meu jogo!!!!!");
         }else{
-            _bot.sendMessage(_bot.messages[i].chat_id, "You rock! You beat my game!!!!!");
+            _bot.sendMessage(_bot.messages[numNewMessages].chat_id, "You rock! You beat my game!!!!!");
         }
        
     }
     
-  }
+  
 }
 
 boolean Bot::vermelho(int chat_id)
 {
     if(_color_read=="vermelho"){
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Parabens!!!!!!");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "vc acertou a cor");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Agora vamos para a proxima cor");
+        const char *congratilations = 
+        "Parabens!!!!!! \ 
+        vc acertou a cor \ 
+        Agora vamos para a proxima cor";
+        _bot.sendMessage(_bot.messages[chat_id].chat_id, congratilations);
         _color_read = nullptr;
         return false;
-    }else{
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Sinto muito");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Essa não é a cor que estou pensando");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "não desista!!! continue tentando");
-        _color_read = nullptr;
-        return true;
     }
+    return true;
 }
 boolean Bot::red(int chat_id)
 {
     if(_color_read=="red"){
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Congratulations!!!!!!!");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "That's exactly the color I was thinking of! ");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Next, let's try another color.");
+        const char *congratilations = 
+        "Parabens!!!!!! \ 
+        vc acertou a cor \ 
+        Agora vamos para a proxima cor";
+        _bot.sendMessage(_bot.messages[chat_id].chat_id, congratilations);
         _color_read = nullptr;
         return false;
-    }else{
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "I'm so sorry.");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "I had a different color in mind.");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Keep going! You can do it!!!");
-        _color_read = nullptr;
-        return true;
     }
+    return true;
 }
 
 boolean Bot::roxo(int chat_id)
 {
     if(_color_read=="roxo"){
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Parabens!!!!!!");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "vc acertou a cor.");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Agora vamos para a proxima cor.");
+        const char *congratilations = 
+        "Parabens!!!!!! \ 
+        vc acertou a cor \ 
+        Agora vamos para a proxima cor";
+        _bot.sendMessage(_bot.messages[chat_id].chat_id, congratilations);
         _color_read = nullptr;
         return false;
-    }else{
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Sinto muito");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Essa não é a cor que estou pensando");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "não desista!!! continue tentando");
-        _color_read = nullptr;
-        return true;
     }
+    return true;
 }
 boolean Bot::purple(int chat_id)
 {
     if(_color_read=="purple"){
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Congratulations!!!!!!!");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "That's exactly the color I was thinking of! ");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Next, let's try another color.");
+       const char *congratilations = 
+        "Parabens!!!!!! \ 
+        vc acertou a cor \ 
+        Agora vamos para a proxima cor";
+        _bot.sendMessage(_bot.messages[chat_id].chat_id, congratilations);
         _color_read = nullptr;
         return false;
-    }else{
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "I'm so sorry.");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "I had a different color in mind.");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Keep going! You can do it!!!");
-        _color_read = nullptr;
-        return true;
     }
+    return true;
 }
 
 boolean Bot::azul(int chat_id)
 {
     if(_color_read=="azul"){
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Parabens!!!!!!");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "vc acertou a cor");
+       const char *congratilations = 
+        "Parabens!!!!!! \ 
+        vc acertou a cor \ 
+        Agora vamos para a proxima cor";
+        _bot.sendMessage(_bot.messages[chat_id].chat_id, congratilations);
         _color_read = nullptr;
         return false;
-    }else{
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Sinto muito");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Essa não é a cor que estou pensando");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "não desista!!! continue tentando");
-        _color_read = nullptr;
-        return true;
     }
+    return true;
 }
 boolean Bot::blue(int chat_id)
 {
     if(_color_read=="blue"){
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Congratulations!!!!!!!");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "That's exactly the color I was thinking of! ");
+       const char *congratilations = 
+        "Parabens!!!!!! \ 
+        vc acertou a cor \ 
+        Agora vamos para a proxima cor";
+        _bot.sendMessage(_bot.messages[chat_id].chat_id, congratilations);
         return false;
-    }else{
-         _bot.sendMessage(_bot.messages[chat_id].chat_id, "I'm so sorry.");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "I had a different color in mind.");
-        _bot.sendMessage(_bot.messages[chat_id].chat_id, "Keep going! You can do it!!!");
-        return true;
     }
+    return true;
 }
 
 void Bot::redGame(int chat_id)
 {
     bool cor_errada = true;
-    _color_read = nullptr;
+    _bot.sendMessage(_bot.messages[chat_id].chat_id, "A cor que eu tenho em mente é comum em frutas e flores");
     
     while(cor_errada)
     {
+        const char* last_color = _color_read;
         
         if(_lang==Language::PORTUGUESE){
             if(_color_read==nullptr){
                 delay(10);
                 continue;
             }
-            _bot.sendMessage(_bot.messages[chat_id].chat_id, "A cor que eu tenho em mente é comum em frutas e flores");
-            cor_errada=vermelho(chat_id);    
+            cor_errada=vermelho(chat_id); 
         }else{
             if(_color_read==nullptr){
                 delay(10);
                 continue;
             }
-            _bot.sendMessage(_bot.messages[chat_id].chat_id, "The color I'm visualizing is typical of many fruits and blooms.");
             cor_errada=red(chat_id);
+            
         }
     }
     
@@ -205,24 +188,25 @@ void Bot::redGame(int chat_id)
 void Bot::purpleGame(int chat_id)
 {
     bool cor_errada = true;
-    _color_read = nullptr;
+    _bot.sendMessage(_bot.messages[chat_id].chat_id, "A cor que eu tenho em mente é muito bonita mas raramente encontrada na naturaza");
 
-    while(cor_errada)
+    while(cor_errada) 
     {
+        const char* last_color = _color_read;
         if(_lang==Language::PORTUGUESE){
             if(_color_read==nullptr){
                 delay(10);
                 continue;
             }
-            _bot.sendMessage(_bot.messages[chat_id].chat_id, "A cor que eu tenho em mente é muito bonita mas raramente encontrada na naturaza");
             cor_errada=roxo(chat_id);
+            
         }else{
             if(_color_read==nullptr){
                 delay(10);
                 continue;
             }
-            _bot.sendMessage(_bot.messages[chat_id].chat_id, "The color I have in mind is a beautiful shade, but it's not often seen in nature.");
             cor_errada=purple(chat_id);
+            
         }
     }
 
@@ -231,23 +215,21 @@ void Bot::purpleGame(int chat_id)
 void Bot::blueGame(int chat_id)
 {
     bool cor_errada = true;
-    _color_read = nullptr;
-
+    _bot.sendMessage(_bot.messages[chat_id].chat_id, "A cor que eu tenho em mente está no lemento base da vida.");
     while(cor_errada)
     {
+        const char* last_color = _color_read;
         if(_lang==Language::PORTUGUESE){
             if(_color_read==nullptr){
                 delay(10);
                 continue;
             }
-            _bot.sendMessage(_bot.messages[chat_id].chat_id, "A cor que eu tenho em mente está no lemento base da vida.");
             cor_errada=azul(chat_id);
         }else{
             if(_color_read==nullptr){
                 delay(10);
                 continue;
             }
-            _bot.sendMessage(_bot.messages[chat_id].chat_id, "The color I'm thinking of is present in the fundamental element of life.");
             cor_errada=blue(chat_id);
         }
     }
